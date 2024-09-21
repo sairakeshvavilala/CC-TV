71809e7f876b0b2c39af8b0edcf7256b128796ad
Sai Rakesh Vavilala <sairakeshvavilala@Sais-Air.alphionsee.in>
2024-09-21 10:00:15 +0530

    local issue resolved

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,17 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAhhldkhGiBQPrzMjlZ1FQi81afu-hCavk",
+  authDomain: "tinder-clone-87918.firebaseapp.com",
+  databaseURL: "https://tinder-clone-87918.firebaseio.com",
+  projectId: "tinder-clone-87918",
+  storageBucket: "tinder-clone-87918.appspot.com",
+  messagingSenderId: "30360611274",
+  appId: "1:30360611274:web:2e49eeb18c417f1c188733"
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
   };
   console.log(process.env.REACT_APP_API_KEY)
   export default firebaseConfig;
